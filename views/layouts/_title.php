1eaec4f8e612588ac979aa79fec355f07db4f977
unknown <suhendra.yohana@gmail.com>
2015-12-31 09:14:15 +0700

    remove unused files

--- views/layouts/_title.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$this->title = $_title;
-$this->params['breadcrumbs'][] = $_breadcrumbs;
-$this->params['_title'] = $_module;
-$this->params['_subtitle'] = $_subModule;
-
-?>
--- views/layouts/footer-copyright.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-	<div class="pull-right hidden-xs"><b>Version</b> 2.3.0</div>
-	<strong>Copyright &copy;  <?php echo $info['copyright']?>
-	<a href="<?php echo $info["url"];?>" target="_blank"><?php echo $info['publisher']?></a>.
-	</strong> All rights reserved || 
-	Developed by : <a href="<?php echo $info["authorurl"];?>" target="_blank"><?php echo $info['author']?></a>
-</footer>
